1bda7adbcfc0492544dc5221a9267a56d8c6757d
ukic <115979046+ukic@users.noreply.github.com>
2023-01-09 18:27:58 +0100

    Update run_translation.py

--- translation/run_translation.py
+++ translation/run_translation.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:translation/my_main.py
 from easynmt import EasyNMT
 
 
@@ -25,7 +24,7 @@ from easynmt import EasyNMT
 # a = translator.translate("That is very satisfying"")
 # print(a)
 
-def Translate_easy(target_lang=str, source_lang=str, text=str) -> str:
+def translate_easy(target_lang=str, source_lang=str, text=str) -> str:
 
     if target_lang == "pl" and source_lang == "en":
         model = EasyNMT('m2m_100_418M')
@@ -33,37 +32,3 @@ def Translate_easy(target_lang=str, source_lang=str, text=str) -> str:
         model = EasyNMT('opus-mt')
     translated = model.translate(text, target_lang, source_lang)
     return translated
-
-=======
-from easynmt import EasyNMT
-
-
-# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-# en_pl_tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-pl-en")
-# en_pl_model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-pl-en")
-#
-# text = "Jak się masz?"
-# encoded_input = en_pl_tokenizer(text, return_tensors="pt")
-# output = en_pl_model.generate(**encoded_input)
-# out_text = en_pl_tokenizer.batch_decode(output, bskip_special_tokens=True)
-# print(out_text)
-
-# import server
-# import json
-# import numpy as np
-#
-# with open('services.json', 'r') as configfile:
-#     service = json.load(configfile)
-#
-# model = np.load(".\models\en-pl\opus.bpe32k-bpe32k.transformer.model1.npz.best-perplexity.npz")
-# translator = server.TranslatorInterface("en", "pl", service, model)
-#
-# a = translator.translate("I have an immense depression")
-# print(a)
-
-def translate_easy(lang=str, text=str) -> str:
-    model = EasyNMT('opus-mt')
-    translated = model.translate(text, target_lang=lang)
-    return translated
-
->>>>>>> gui:translation/run_translation.py
